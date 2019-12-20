6533643bbf0d082390d7021615e3df3859eff472
WangRan <ranwang1995@outlook.com>
2019-12-21 02:32:08 +0800

    [WangRan] fix: correct the style and naming

--- js/index.js
+++ js/index.js
@@ -43,29 +43,62 @@ var carProducts = [
     "checked": false
   }
 ]
-var shoppingTable = document.getElementsByClassName("shopping-table")[0]; 
+
+var shoppingTable = document.getElementsByClassName("shopping-table")[0];
 var shoppingList = document.getElementsByClassName("shopping-list")[0];
 var goodCount = document.getElementsByClassName("good-count")[0];
 var totalCost = document.getElementsByClassName("total-cost")[0];
 var selectAll = document.getElementsByClassName("select-all")[0];
 var cellName = ['checked', 'name', 'price', 'count', 'good-cost'];
-var cellQuery = ['checked', 'name', 'price', 'count', 'id'];
 var isSelectAllChecked = false;
 
-for (var index = 0; index < carProducts.length; index++) {
-  // var newRow = createRow('shopping-list');
-  var cellIndex = 0;
-  var newRow = createRow(shoppingList);
-  newRow.setAttribute('row-id', carProducts[index]['id']);
-  for (var iter = 0, max = Object.getOwnPropertyNames(carProducts[index]).length; iter < max; iter++) {
-    cellIndex++;
-    var newCell = createCell(newRow);
-    fillCell(newCell, cellIndex, carProducts[index][cellQuery[iter]]);
-    // newCell.setAttribute('class', carProducts[index]['id']+' '+cellName[iter]);
-    newCell.setAttribute('class', cellName[iter]);
+initialList();
+updateList();
+shoppingTable.addEventListener('click', clickHandle);
+
+function initialList() {
+  for (var index = 0; index < carProducts.length; index++) {
+    var cellIndex = 0;
+    var newRow = createRow(shoppingList);
+    var cellQuery = ['checked', 'name', 'price', 'count', 'id'];
+    newRow.setAttribute('row-id', carProducts[index]['id']);
+    for (var iter = 0, max = Object.getOwnPropertyNames(carProducts[index]).length; iter < max; iter++) {
+      cellIndex++;
+      var newCell = createCell(newRow);
+      fillCell(newCell, cellIndex, carProducts[index][cellQuery[iter]]);
+      newCell.setAttribute('class', cellName[iter]);
+    }
   }
 }
 
+function updateList() {
+  setSelectAllCheckStatus();
+  setGoodCheckStatus();
+  setGoodCost();
+  goodCount.textContent = setGoodCount();
+  totalCost.textContent = setTotalCost()
+}
+
+function clickHandle(event) {
+  var currentClass = event.target.getAttribute('class')
+    || event.target.parentElement.getAttribute('class')
+    || event.target.parentElement.parentElement.getAttribute('class');
+  var rowId = event.target.parentElement.getAttribute('row-id')
+    || event.target.parentElement.parentElement.getAttribute('row-id')
+    || event.target.parentElement.parentElement.parentElement.getAttribute('row-id');
+  switch (currentClass) {
+    case 'select-all':
+      onClickSelectAll();
+      break;
+    case 'checked':
+      onClickGoodChecked(rowId);
+      break;
+    case 'math-icon':
+      onClickMathIcon(rowId, event.target);
+      break;
+  }
+  updateList();
+}
 
 function createRow(tbodyNode) {
   var newRow = document.createElement('tr');
@@ -101,19 +134,21 @@ function createCheckBox() {
   checkBox.setAttribute('type', 'checkbox');
   return checkBox;
 }
+
 function createMinusIcon() {
-  // var quantityCell = document.createElement('div');
   var minusIcon = document.createElement('div');
   minusIcon.textContent = '-';
   minusIcon.setAttribute('class', 'math-icon');
   return minusIcon;
 }
+
 function createPlusIcon() {
   var plusIcon = document.createElement('div');
   plusIcon.textContent = '+';
   plusIcon.setAttribute('class', 'math-icon');
   return plusIcon;
 }
+
 function createQuantityBox(content) {
   var quantityBox = document.createElement('div');
   quantityBox.textContent = content;
@@ -122,33 +157,21 @@ function createQuantityBox(content) {
 }
 
 function setSelectAllCheckStatus() {
-  if (isSelectAllChecked === true) {
+  if (true === isSelectAllChecked) {
     selectAll.setAttribute('checked', 'checked');
-  }
-  else {
+  } else {
     selectAll.removeAttribute('checked');
   }
 }
 
 function setGoodCheckStatus() {
-  var checkBoxEls = document.getElementsByClassName(cellQuery[0]);
-  for (var ind = 0; ind < checkBoxEls.length; ind++) {
-    // console.log(carProducts[checkBoxEls[ind].parentElement.getAttribute("row-id")-1]["checked"]);
-    if (carProducts[checkBoxEls[ind].parentElement.getAttribute("row-id") - 1]["checked"] === true) {
-      checkBoxEls[ind].firstChild.setAttribute('checked', 'checked');
+  var checkBoxEls = document.getElementsByClassName(cellName[0]);
+  for (var index = 0; index < checkBoxEls.length; index++) {
+    if (true === carProducts[checkBoxEls[index].parentElement.getAttribute("row-id") - 1][cellName[0]]) {
+      checkBoxEls[index].firstChild.setAttribute('checked', 'checked');
+    } else {
+      checkBoxEls[index].firstChild.removeAttribute('checked');
     }
-    else {
-      checkBoxEls[ind].firstChild.removeAttribute('checked');
-    }
-  }
-}
-
-function setItemCost() {
-  var rowEls = shoppingList.children;
-  for (var index = 0; index < rowEls.length; index++) {
-    var rowId = rowEls[index].getAttribute('row-id');
-    rowEls[index].getElementsByClassName(cellName[4])[0].textContent =
-      carProducts[rowId - 1][cellQuery[2]] * carProducts[rowId - 1][cellQuery[3]];
   }
 }
 
@@ -157,7 +180,7 @@ function setGoodCost() {
   for (var index = 0; index < rowEls.length; index++) {
     var rowId = rowEls[index].getAttribute('row-id');
     rowEls[index].getElementsByClassName(cellName[4])[0].textContent =
-      carProducts[rowId - 1][cellQuery[2]] * carProducts[rowId - 1][cellQuery[3]];
+      carProducts[rowId - 1][cellName[2]] * carProducts[rowId - 1][cellName[3]];
   }
 }
 
@@ -166,11 +189,11 @@ function setGoodCount() {
   var sum = 0;
   for (var index = 0; index < rowEls.length; index++) {
     var rowId = rowEls[index].getAttribute('row-id');
-    if (carProducts[rowId - 1][cellQuery[0]] === true) {
-      sum += carProducts[rowId - 1][cellQuery[3]];
+    if (true === carProducts[rowId - 1][cellName[0]]) {
+      sum += carProducts[rowId - 1][cellName[3]];
     }
   }
-  goodCount.textContent = sum;
+  return sum;
 }
 
 function setTotalCost() {
@@ -178,60 +201,29 @@ function setTotalCost() {
   var sum = 0;
   for (var index = 0; index < goodCostEls.length; index++) {
     var rowId = goodCostEls[index].parentElement.getAttribute('row-id');
-    if (carProducts[rowId - 1][cellQuery[0]] === true) {
+    if (true === carProducts[rowId - 1][cellName[0]]) {
       sum += (+goodCostEls[index].textContent);
     }
   }
-  totalCost.textContent = sum;
-}
-
-function updateList() {
-  setSelectAllCheckStatus();
-  setGoodCheckStatus();
-  setGoodCost();
-  setTotalCost()
-  setGoodCount();
-}
-
-updateList();
-shoppingTable.addEventListener('click',clickHandle);
-function clickHandle(event) {
-  var currentClass = event.target.getAttribute('class')
-    || event.target.parentElement.getAttribute('class')
-    || event.target.parentElement.parentElement.getAttribute('class');
-  var rowId = event.target.parentElement.getAttribute('row-id')
-    || event.target.parentElement.parentElement.getAttribute('row-id')
-    || event.target.parentElement.parentElement.parentElement.getAttribute('row-id');
-  console.log(event.target);
-    console.log(currentClass);
-  console.log(rowId);
-  switch (currentClass) {
-    case 'select-all':
-      onClickSelectAll();
-      break;
-    case 'checked':
-      onClickGoodChecked(rowId);
-      break;
-    case 'math-icon':
-      onClickMathIcon(event.target.innerHTML,rowId,event.target);
-      break;
-  }
-  updateList();
+  return sum;
 }
 
 function onClickSelectAll() {
-  if (isSelectAllChecked === false) {
+  if (false === isSelectAllChecked) {
     isSelectAllChecked = true;
   }
   else {
     isSelectAllChecked = false;
   }
+  resetGoodCheckBox();
+}
+
+function resetGoodCheckBox() {
   var rowEls = shoppingList.children;
-  for (var index = 0; index < rowEls.length; index++) {
+  for (var index = 0; index < carProducts.length; index++) {
     carProducts[index][cellName[0]] = isSelectAllChecked;
-    
   }
-  var checkBoxEls=document.getElementsByClassName(cellName[0]);
+  var checkBoxEls = document.getElementsByClassName(cellName[0]);
   for (var index = 0; index < checkBoxEls.length; index++) {
     checkBoxEls[index].removeChild(checkBoxEls[index].firstChild);
     checkBoxEls[index].appendChild(createCheckBox());
@@ -239,15 +231,15 @@ function onClickSelectAll() {
 }
 
 function onClickGoodChecked(rowId, checkBoxEl) {
-  if (carProducts[rowId - 1][cellName[0]] === false) {
+  if (false === carProducts[rowId - 1][cellName[0]]) {
     carProducts[rowId - 1][cellName[0]] = true;
   } else {
     carProducts[rowId - 1][cellName[0]] = false;
-    renewSelectAllCheckBox();
+    resetSelectAllCheckBox();
   }
 }
 
-function renewSelectAllCheckBox(){
+function resetSelectAllCheckBox() {
   isSelectAllChecked = false;
   var selectAllParentEl = selectAll.parentElement;
   selectAllParentEl.removeChild(selectAll);
@@ -256,15 +248,15 @@ function renewSelectAllCheckBox(){
   selectAll.setAttribute('class', 'select-all');
 }
 
-function onClickMathIcon(operator,rowId,target){
-  if (operator === '+'){
-    event.target.parentElement.getElementsByClassName('quantity-box')[0].innerHTML=++carProducts[rowId - 1][cellName[3]];
-  } else{
-    event.target.parentElement.getElementsByClassName('quantity-box')[0].innerHTML=--carProducts[rowId - 1][cellName[3]];
-    if (carProducts[rowId - 1][cellName[3]] <= 0)
-    {
-      shoppingList.removeChild(target.parentElement.parentElement)
+function onClickMathIcon(rowId, target) {
+  if (target.innerHTML === '+') {
+    target.parentElement.getElementsByClassName('quantity-box')[0].innerHTML
+      = ++carProducts[rowId - 1][cellName[3]];
+  } else {
+    target.parentElement.getElementsByClassName('quantity-box')[0].innerHTML
+      = --carProducts[rowId - 1][cellName[3]];
+    if (carProducts[rowId - 1][cellName[3]] <= 0) {
+      shoppingList.removeChild(target.parentElement.parentElement);
     }
   }
 }
-//删除商品时要取消选中
